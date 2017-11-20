c77aeec29463dbc772452bcce715334a330a9c87
RobertScanlon <scanlonr@oregonstate.edu>
2017-11-19 19:14:17 -0500

    added make people/pets scripts, create main android layout

---
+++ makepeople.py
@@ -0,0 +1,26 @@
+import requests
+import json
+
+new_pet_url = "http://localhost:8080/pet"
+new_person_url = "http://localhost:8080/person"
+
+class people:
+    def __init__(self, fn, ln, age, addr):
+        self.fname = fn
+        self.lname = ln
+        self.age = age
+        self.address = addr
+    fname = ""
+    lname = ""
+    age = 0
+    address = ""
+
+p1 = people("Robert", "Scanlon", 25, "Huntington, NY")
+p2 = people("Cathleen", "Scanlon", 61, "Huntington, NY")
+p3 = people("Jim", "Scanlon", 10, "Westbury, NY")
+people = [p1, p2, p3]
+
+for p in people:
+    j = json.dumps(p.__dict__)
+    r = requests.post(new_person_url, j)
+    print(r.text)
---
+++ makepets.py
@@ -0,0 +1,27 @@
+import requests
+import json
+
+new_pet_url = "http://localhost:8080/pet"
+
+class pet:
+    def __init__(self, n, s, a, w):
+        self.name = n
+        self.species = s
+        self.age = a
+        self.weight = w
+    name = ""
+    species = ""
+    age = 0
+    weight = 0
+
+pt1 = pet("Hazel", "Cat", 5, 10) 
+pt2 = pet("Fallon", "Cat", 5, 9) 
+pt3 = pet("Freddy", "Cat", 6, 12) 
+pt4 = pet("Rocky", "Dog", 4, 20) 
+pt5 = pet("Duchess", "Dog", 6, 22) 
+pets = [pt1, pt2, pt3, pt4, pt5]
+
+for p in pets:
+    j = json.dumps(p.__dict__)
+    r = requests.post(new_pet_url, j)
+    print(r.text)
